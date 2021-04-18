2e36bdbd32605fa743749f2b8b76107a68e40868
kasaderos <kb.kaldarov@yandex.ru>
2021-04-19 03:34:30 +0600

    iters: Add stable chebyshev method

--- 2_stable_chebyshev.py
+++ 2_stable_chebyshev.py
@@ -43,7 +43,9 @@ print('roH = ', roH)
 
 err = norm(u-ua)
 eps = 1e-5
-m = round(np.log(2/eps)/(2*np.sqrt(xi))) + 2
+m = round(np.log(2/eps)/(2*np.sqrt(xi))) + 4
+if m & 1:
+    m += 1
 print('optimal m = ', m)
 print('h^2 = ', h*h)
 print('eps = ', eps)
@@ -54,12 +56,38 @@ for i in range(N1):
     u[0, i] = mu(0.0, y[i]) 
     u[N, i] = mu(1.0, y[i])
 
-tau = np.array([2 /  (delta2 + delta + (delta2-delta)*np.cos((2*k-1) * np.pi / (2*N))) for k in range(1, N+1)])
+def get_teta(n):
+    n = pow_two(n)
 
-for k in range(1, m+1):
+    teta = { 1:[1] }
+
+    i = 2
+    while i <= n:
+        new_teta = []
+        j = 1
+        for j in range(1, i+1):
+            if j & 1:   # odd
+                new_teta.append(teta[ i//2 ][ (j+1)//2 -1 ])
+            else:       # even
+                new_teta.append(2*i - new_teta[j-2])
+        teta[i] = new_teta
+        i *= 2
+    return teta[n]
+
+def pow_two(n):
+    i = 1
+    while i < n:
+        i *= 2
+    return i
+
+teta = get_teta(m)
+
+tau = np.array([2 /  (delta2 + delta + (delta2-delta)*np.cos(teta[k] * np.pi / (2 * N))) for k in range(m)])
+
+for k in range(m):
     for i in range(1, N):
         for j in range(1, N):
-            u[i, j] = u_old[i, j] + tau[k%N]*((u_old[i+1, j] - u_old[i, j]) / h2 - (u_old[i, j] - u_old[i-1, j]) / h2 + 
+            u[i, j] = u_old[i, j] + tau[k]*((u_old[i+1, j] - u_old[i, j]) / h2 - (u_old[i, j] - u_old[i-1, j]) / h2 + 
                     (u_old[i, j+1] - u_old[i, j]) / h2 - (u_old[i, j] - u_old[i, j-1]) / h2 + f(x[i], y[j])) 
     
     u_old = u.copy()
